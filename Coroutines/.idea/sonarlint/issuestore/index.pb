6e72227aab3a7c6d9e167d4431336b9d38f09aed
betabear <betabear26@gmail.com>
2021-08-21 18:49:34 +0530

    Basics

---
+++ Coroutines/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+m
+=app/src/main/java/dev/sandeepsuman/coroutines/MainActivity.kt,0/1/01e7f021c256be9aa0f0589b3b1408c2f1361ad0
--- Coroutines/app/src/main/java/dev/sandeepsuman/coroutines/basics/BasicsActivity.kt
+++ Coroutines/app/src/main/java/dev/sandeepsuman/coroutines/basics/BasicsActivity.kt
@@ -1,29 +1,72 @@
 package dev.sandeepsuman.coroutines.basics
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import dev.sandeepsuman.coroutines.R
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 class BasicsActivity : AppCompatActivity() {
 
-    lateinit var textView: TextView
-    lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var buttonSimple: Button
+    private lateinit var buttonScope: Button
+    private lateinit var buttonScopeInParallel: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basics)
 
         textView = findViewById(R.id.text)
-        button = findViewById(R.id.btnBasics)
+        buttonSimple = findViewById(R.id.btnSimple)
+        buttonScope = findViewById(R.id.btnScope)
+        buttonScopeInParallel = findViewById(R.id.btnScopeParallel)
 
-        button.setOnClickListener {
+        //Blocks the UI
+        buttonSimple.setOnClickListener {
             workSimple()
+        }
+
+        //This does not block the UI Thread but multiple calls to function runs in sequence
+        buttonScope.setOnClickListener {
+            GlobalScope.launch {
+                scopeWork()
+                scopeWork()
+                scopeWork()
+                scopeWork()
+                scopeWork()
+            }
+        }
+
+        //Does not block UI and runs parallel
+        buttonScopeInParallel.setOnClickListener {
+            GlobalScope.launch {
+                launch { scopeWork() }
+                launch { scopeWork() }
+                launch { scopeWork() }
+                launch { scopeWork() }
+                launch { scopeWork() }
+            }
+        }
+
+        //This blocks the main thread
+       /* runBlocking {
+            launch {
+                delay(10000L)
+                textView.append("\n${System.currentTimeMillis()}")
+            }
+        }*/
+
+        //This won't block the main thread
+        GlobalScope.launch {
+            withContext(Dispatchers.IO){
+                delay(10000L)
+                runOnUiThread {
+                    textView.append("This")
+                }
+            }
         }
 
     }
@@ -32,12 +75,19 @@ class BasicsActivity : AppCompatActivity() {
         launch {
             delay(1000L)
             runOnUiThread {
-                textView.text = System.currentTimeMillis().toString()
+                textView.append("\n${System.currentTimeMillis()}")
             }
         }
     }
 
     suspend fun scopeWork() = coroutineScope {
+
+        launch(Dispatchers.IO) {
+            delay(10000L)
+            runOnUiThread {
+                textView.append("\n${System.currentTimeMillis()}")
+            }
+        }
 
     }
 }
